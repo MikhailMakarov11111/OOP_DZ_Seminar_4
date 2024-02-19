8ec76ec67264961f52fc3b10e89d8ec8ce4967d2
Your Name <mikhailmakarov92@gmail.com>
2024-02-19 06:09:28 +0400

    Добавлены функции бинарного перевода

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,16 +3,14 @@ package org.example;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Calculator {
 
-    public double sum(List<? extends Number> numbers){
+    public double sum(List<? extends Number> numbers) {
         double s = 0;
 
         for (Number number : numbers) {
@@ -21,7 +19,7 @@ public class Calculator {
         return s;
     }
 
-    public double composition(List<? extends Number> numbers){
+    public double composition(List<? extends Number> numbers) {
         double com = 1;
 
         for (Number number : numbers) {
@@ -30,12 +28,44 @@ public class Calculator {
         return com;
     }
 
-    public double quotient(List<? extends Number> numbers){
-        double quo = numbers.indexOf(0);
-
-        for (int i = 1; i <numbers.size(); i++) {
-            quo /= numbers.indexOf(i);
+    public double quotient(List<? extends Number> numbers) {
+        double quo = numbers.get(0).doubleValue();
+        for (int i = 1; i < numbers.size(); i++) {
+            if (numbers.get(i).doubleValue() == 0.0) {
+                throw new ArithmeticException("Деление на ноль запрещено!");
+            }
+            quo /= numbers.get(i).doubleValue();
         }
         return quo;
     }
+
+    public static int binaryToDecimal(int num) {
+        String binaryString = String.valueOf(num);
+        if (!binaryString.matches("[01]+")) {
+            throw new IllegalArgumentException("Неверный формат двоичного числа");
+        }
+
+        int decimalNumber = 0;
+        int base = 1;
+
+        while (num > 0) {
+            int lastDigit = num % 10;
+            num = num / 10;
+            decimalNumber += lastDigit * base;
+            base = base * 2;
+        }
+
+        return decimalNumber;
+    }
+
+    public static int binaryStringToDecimal(String binaryString) {
+        if (!binaryString.matches("[01]+")) {
+            throw new IllegalArgumentException("Неверный формат двоичного числа");
+        }
+
+        int binaryNumber = Integer.parseInt(binaryString, 2);
+        return binaryNumber;
+    }
+
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,14 +7,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        while (true) {
+        boolean flag = true;
+        while (flag) {
             System.out.println("Добро пожаловать в Калькулятор, для того чтобы \n " +
                     "продолжить выберите дейтствие:\n" +
                     "1. Сложение; \n" +
                     "2. Умножение; \n" +
                     "3. Деление; \n" +
-                    "4. Бинарный перевод; \n" +
-                    "5. Закрыть приложение");
+                    "4. Бинарный перевод чисел; \n" +
+                    "5. Бинарный перевод строк; \n" +
+                    "6. Закрыть приложение");
             Scanner sc = new Scanner(System.in);
             switch (sc.nextInt()) {
                 case 1:
@@ -23,6 +25,7 @@ public class Main {
                     List<Double> list1 = numbers(sizeNum, sc);
                     Calculator summa = new Calculator();
                     System.out.println("Сумма введнных Вами чисел равна " + summa.sum(list1));
+                    flag = false;
                     break;
                 case 2:
                     System.out.println("Введите колличество чисел которые вы хотите перемножить: ");
@@ -31,6 +34,7 @@ public class Main {
                     Calculator com = new Calculator();
                     System.out.println("Произведение введнных Вами чисел равно "
                             + com.composition(list2));
+                    flag = false;
                     break;
                 case 3:
                     System.out.println("Введите колличество чисел которые вы хотите поделить: ");
@@ -39,27 +43,50 @@ public class Main {
                     Calculator quo = new Calculator();
                     System.out.println("Частное введнных Вами чисел равно "
                             + quo.quotient(list3));
+                    flag = false;
                     break;
                 case 4:
+                    System.out.println("Введите число в двочиной системе, которое вы хоите перевети " +
+                            "в десятичную : ");
+                    Scanner scNum = new Scanner(System.in);
+                    int numBin = scNum.nextInt();
+                    Calculator numBinar = new Calculator();
+                    System.out.println("Введенное Вами число в десятичной системе = " +
+                            numBinar.binaryToDecimal(numBin));
+                    scNum.close();
+                    flag = false;
                     break;
                 case 5:
+                    System.out.println("Введите строку в двочиной системе, которое вы хоите перевети " +
+                            "в десятичную : ");
+                    Scanner scWord = new Scanner(System.in);
+                    String word = scWord.next();
+                    Calculator wordBin = new Calculator();
+                    System.out.println("Введенная Вами строка в десятичной системе: " +
+                            wordBin.binaryStringToDecimal(word));
+                    scWord.close();
+                    flag = false;
+                    break;
+                case 6:
                     System.out.println("Спасибо что воспользовались нашим приложение!\n" +
                             "Ждем вас снова!");
+                    flag = false;
+                    break;
                 default:
                     System.out.println("Вы ввели некорректное значение, пожалуйста попробуйте еще раз \n" +
-                            "или введите цифру 5 чтобы выйти из приложения");;
+                            "или введите цифру 5 чтобы выйти из приложения");
+                    ;
             }
-        }
-
-
-        }
-
-        private static List<Double> numbers ( int size, Scanner sc){
-            List<Double> list = new ArrayList<>();
-            System.out.println("Введите " + size + " чисел: ");
-            for (int i = 0; i < size; i++) {
-                list.add(sc.nextDouble());
-            }
-            return list;
+            sc.close();
         }
     }
+
+    private static List<Double> numbers(int size, Scanner sc) {
+        List<Double> list = new ArrayList<>();
+        System.out.println("Введите " + size + " чисел: ");
+        for (int i = 0; i < size; i++) {
+            list.add(sc.nextDouble());
+        }
+        return list;
+    }
+}
